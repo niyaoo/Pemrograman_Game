aedd7279e93cbd57f4c45d68e443c9b3b0a86325
niyaoo <duarsatamara@gmail.com>
2020-09-18 20:15:48 +0800

    Basic Scripting

---
+++ BasicScripting/Assets/Latihan3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		int c = Jumlah (60, 40);
+		Debug.Log ("Hasil Jumlah a dan b adalah " + c);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+			{
+				
+			}
+
+///<summary> 
+/// Jumlah the specifield a and b.
+///<summary> 
+///Merupakan fungsi penjumlahan dari 2 input angka
+///</summary> 
+/// <param name="a" >Masukan Angka Integer</param>  
+/// <param name="b" >Masukan Angka Integer</param> 
+			int Jumlah (int a, int b)
+			{
+		return a + b;
+			}
+		}
