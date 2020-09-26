f4321bfffdb0f4028e3b122b948b73fcea9fc0c6
niyaoo <duarsatamara@gmail.com>
2020-09-26 20:46:01 +0800

    Update

---
+++ BasicScripting/Assets/Latihan4/Latihan4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan4 : MonoBehaviour 
+{
+	Karakter player1;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player1 = new Karakter ();
+		player1.Name = "Dico";
+		player1.Health = 100;
+		player1.Damage = 30;
+
+		Debug.Log ("Name : " + player1.Name + ", Health : " + player1.Health + ", Demage : " + player1.Damage);
+
+		player1.Jump ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ BasicScripting/Assets/Latihan5/Prajurit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prajurit : Manusia {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log ("Seorang Prajurit dapat : ");
+		Makan ();
+		Tidur ();
+		Menyerang ();
+	}
+
+	void Menyerang() 
+	{
+		Debug.Log ("Menyerang");
+	}
+}
