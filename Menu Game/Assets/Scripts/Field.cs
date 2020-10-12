a2705a81e459a39297e09e7e24134f149f31f23d
niyaoo <duarsatamara@gmail.com>
2020-10-12 22:02:37 +0800

    Menu

---
+++ Menu Game/Assets/Scripts/Field.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Field : MonoBehaviour 
+{
+
+	//Input nama Anda pada field
+	public InputField nama;
+
+	//Input hewan kesukaan pada field
+	public InputField hewan;
+
+	//menampilan teks
+	public Text teks;
+
+	public void TeksField()
+	{
+		if 
+			(nama.text=="" || hewan.text=="")
+		{
+			teks.text = "Lengkapilah kolom terlebih dahulu...";
+		}
+		else
+		{
+			teks.text = "Namaku " + nama.text + ", dan Hewan kesukaanku " + hewan.text;
+		}
+	}
+}
